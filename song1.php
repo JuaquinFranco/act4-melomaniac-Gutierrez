2f43ad47da2508d54639c553a39a61deb315379b
GutierrezJuaquinFranco <juaquin.gutierrez02@gmail.com>
2022-04-11 15:33:12 +0800

    song1

---
+++ song1.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Index Page</title>
+        <meta charset="utf-8" />
+        <meta http-equiv+X-UA-Compatible content="IE=edge">
+        <meta name+vewport content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/style.css">
+      
+        <link href="css/styles.css" rel="stylesheet" />
+</head>
+
+<body style='background-color:lightgray;'>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Eargasm</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item index">
+        <a class="nav-link" href="index.php">Home <span class="sr-only"></span></a>
+      </li>
+
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Songs
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="song1.php">Song 1</a>
+          <a class="dropdown-item" href="song2.php">Song 2</a>
+          <a class="dropdown-item" href="song3.php">Song 3</a>
+          <a class="dropdown-item" href="song4.php">Song 4</a>
+          <a class="dropdown-item" href="song5.php">Song 5</a>
+        </div>
+      </li>
+            <li class="nav-item">
+        <a class="nav-link" href="contact.php">Contact</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+<br>
+<br>
+       <header class="bg-dark text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">EARGASM</h1>
+                <p class="lead">Eargasm is a developing music website and is still in beta. Enjoy browsing songs with its chords.</p>
+                
+            </div>
+        </header>
+<br><br>
+
+ <section id="songs">
+            <div class="container px-4">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        
+                        <?php
+
+                        echo "<h2 class='text-dark'>SOMEBODY</h2>";
+                        echo "<p class='lead '>keshi</p>";
+                        echo '(chords can be found in <a href="http://chordsworld.com/keshi-somebody-chords/">this website)</a>';
+                        echo "<br/>";
+                        echo "<br/>";
+                        echo "<br/>";
+                
+                        echo "<p> Baby, why the hesitation?</p>";
+                        echo "<p> 'Cause I know that you can feel it too</p>";
+                        echo "<p> You want more and I can taste it</p>";
+                        echo "<p> Let me show you what I wanna do</p>";
+                        echo "<p> Tryna get trapped in your leg lock</p>";
+                        echo "<p> Go another round, make the bed rock</p>";
+                        echo "<p> Yuh, bite the neck, I'ma make it sweat</p>";
+                        echo "<p> Never give you no rest, never give you no rest</p>";
+                        echo "<br/>";
+
+                        echo "<p> You needed somebody, baby, put it on me</p>";
+                        echo "<p> I can be that body, said I can be that body</p>";
+                        echo "<p> You needed somebody, baby, put it on me</p>";
+                        echo "<p> I can be that body, said I can be that, no</p>";
+                        echo "<br/>";
+
+                        echo "<p> [Chorus]</p>";
+                        echo "<p> Baby, it’s you, baby, it’s you</p>";
+                        echo "<p> (Fuck around, put you down, get it loud, nother round</p>";
+                        echo "<p> Baby, it’s you,</p>";
+                        echo "<p> Tell me where you want, te—tell me where you want it</p>";
+                        echo "<p> Baby, it’s you, baby, it’s you</p>";
+                        echo "<p> (Fuck around, put you down, get it loud, 'nother round)</p>";
+                        echo "<p> Baby, it’s you</p>";
+                        echo "<p> (You needed somebody, baby, put it on me)</p>";
+                        echo "<p> Tell me where you want, te—tell me where you want it</p>";
+                        echo "<br/>";
+                        
+                       echo "<p> Back it up, girl, now you speakin' my language</p>";
+                       echo "<p> Show me your favorite spots, but you don’t gotta say it</p>";
+                       echo "<p> (Pull me close, hold it tight, let me go through the night)</p>";
+                       echo "<p> Saddle up girl, why you keepin' me waitin'?</p>";
+                       echo "<p> Hold on if you wanna get off</p>";
+                       echo "<p> If you want love, we gon' make it</p>";
+                       echo "<p> (Love, we gon' make— lo—lo—love, we gon' make i—it)</p>";
+                       echo "<br/>";
+
+                       echo "<p> You needed somebody, baby, put it on me</p>";
+                       echo "<p> I can be that body, said I can be that body</p>";
+                       echo "<p> You needed somebody, baby, put it on me</p>";
+                       echo "<p> I can be that body, said I can be that, no</p>";
+                       echo "<br/>";
+
+                       echo "<p> [Chorus]</p>";
+                       echo "<p> Baby, it’s you, baby, it’s you</p>";
+                       echo "<p> (Fuck around, put you down, get it loud, nother round</p>";
+                       echo "<p> Baby, it’s you,</p>";
+                       echo "<p> Tell me where you want, te—tell me where you want it</p>";
+                       echo "<p> Baby, it’s you, baby, it’s you</p>";
+                       echo "<p> (Fuck around, put you down, get it loud, 'nother round)</p>";
+                       echo "<p> Baby, it’s you</p>";
+                       echo "<p> (You needed somebody, baby, put it on me)</p>";
+                       echo "<p> Tell me where you want, te—tell me where you want it</p>";
+                       echo "<br/>";
+
+                       echo "<p> Pull me close</p>";
+                       echo "<p> Pull me close</p>";
+                       echo "<p> Hold it tight</p>";
+                       echo "<p> Pull me—pull me close</p>";
+                       echo "<p> Pull me close</p>";
+                       echo "<p> Pull me close</p>";
+                       echo "<p> Hold it tight</p>";
+                       echo "<p> Pull me—pull me close</p>";
+
+
+                        ?>
+
+
+                    </div>
+                </div>
+            </div>
+        </section>
+        
+ <footer class="py-4 bg-dark">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Juaquin Franco Gutierrez</p></div>
+        </footer>
+
+
+ <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+</body>
+
+</body>
+</html>
