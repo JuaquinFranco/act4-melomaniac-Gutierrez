5308b77869772d028ee56f70d746ca941d7e5951
GutierrezJuaquinFranco <juaquin.gutierrez02@gmail.com>
2022-04-11 14:40:34 +0800

    index

---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Index Page</title>
+		<meta charset="utf-8" />
+        <meta http-equiv+X-UA-Compatible content="IE=edge">
+        <meta name+vewport content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/style.css">
+      
+        <link href="css/styles.css" rel="stylesheet" />
+</head>
+
+<body style='background:url(bg1.jpg); background-repeat: no-repeat; background-size: cover; background-blend-mode: lighten;'>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Eargasm</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Songs
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="song1.php">Song 1</a>
+          <a class="dropdown-item" href="song2.php">Song 2</a>
+          <a class="dropdown-item" href="song3.php">Song 3</a>
+          <a class="dropdown-item" href="song4.php">Song 4</a>
+          <a class="dropdown-item" href="song5.php">Song 5</a>
+        </div>
+      </li>
+            <li class="nav-item">
+        <a class="nav-link" href="contact.php">Contact</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+<br><br>
+       <header class="bg-dark text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">EARGASM</h1>
+                <p class="lead">Eargasm is a developing music website and is still in beta. Enjoy browsing songs.</p>
+                
+            </div>
+        </header>
+<br><br>
+                <header class="bg-dark text-white">
+                <div class="text-center">
+                <h1>Artists</h1> <br>
+              </header>
+
+                <center>
+                <a href="https://twitter.com/keshimusic"><img src="keshi.jpg" width="450" height="450"/>
+                <a href="https://twitter.com/DanielCaesar"><img src="dc.jpg" width="400" height="450"/>
+                <a href="https://twitter.com/keun16308352"><img src="ken.jpg" width="450" height="450"/>
+                <a href="https://twitter.com/tellemjaye"><img src="james.jpg" width="450" height="450"/>
+                <a href="https://twitter.com/henrylau89"><img src="henry.jpg" width="400" height="450"/>
+                </center>
+
+
+<br><br>
+ <footer class="py-4 bg-dark">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Juaquin Franco Gutierrez</p></div>
+        </footer>
+
+ <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+</body>
+
+</body>
+</html>
